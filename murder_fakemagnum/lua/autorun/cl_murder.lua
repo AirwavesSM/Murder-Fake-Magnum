184ce36e4e27e7ea761fb6eef75e8001383bf2c3
Alex <112224158+AirwavesSM@users.noreply.github.com>
2022-12-12 16:09:00 +0300

    added halo for fake magnum

--- murder_fakemagnum/lua/autorun/cl_murder.lua
+++ murder_fakemagnum/lua/autorun/cl_murder.lua
@@ -1,7 +1,18 @@
--- Next Version
+HaloRenderFakeMagnum = CreateClientConVar( "mu_halo_fakemagnum", 1, true, true )
 
---hook.Add("PreDrawMurderHalos", "DrawHalo", function(Add)
---	if GAMEMODE_NAME == "murder" then
---		
---	end
---end)
+hook.Add("PreDrawMurderHalos", "DrawHalo", function(Add)
+	local client = LocalPlayer()
+	local halos = {}
+	if GAMEMODE_NAME == "murder" then
+		if IsValid(client) && client:Alive() && GAMEMODE.HaloRender:GetBool() then
+			if HaloRenderFakeMagnum:GetBool() then
+				for k, v in ipairs(ents.FindByClass("weapon_mu_fakemagnum")) do
+					if !IsValid(v.Owner) then
+						table.insert(halos, {ent = v, color = 1})
+					end
+				end
+			end
+		end
+	Add(halos, {Color(0, 0, 255)}, 5, 5, 5, true, false)
+	end
+end)
